536e7c1d1902a3134bfee45aad66bcc426fc79eb
Marek Goldmann <mgoldman@redhat.com>
2018-09-10 18:58:12 +0200

    Initial import of ARQ tests

---
+++ tests/arq/src/test/java/org/jboss/test/arquillian/ce/amq/Amq63VirtualTopicSubscriberMigrationTest.java
@@ -0,0 +1,62 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016 Red Hat Inc. and/or its affiliates and other
+ * contributors as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.test.arquillian.ce.amq;
+
+import java.io.IOException;
+
+
+import org.arquillian.cube.openshift.api.*;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.test.arquillian.ce.amq.support.AmqMigrationTestBase;
+import org.jboss.test.arquillian.ce.amq.support.AmqVirtualTopicSubscriberMigrationTestBase;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Ales Justin
+ */
+@RunWith(Arquillian.class)
+@Template(url = "https://raw.githubusercontent.com/${template.repository:jboss-openshift}/application-templates/${template.branch:master}/amq/amq63-persistent.json",
+        parameters = {
+                @TemplateParameter(name = "IMAGE_STREAM_NAMESPACE", value = "${kubernetes.namespace:openshift}"),
+                @TemplateParameter(name = "MQ_QUEUES", value = "QUEUES.FOO,QUEUES.BAR"),
+                @TemplateParameter(name = "MQ_TOPICS", value = "topics.mqtt,TOPICS.FOO"),
+                @TemplateParameter(name = "APPLICATION_NAME", value = "amq-test"),
+                @TemplateParameter(name = "MQ_USERNAME", value = "${amq.username:amq-test}"),
+                @TemplateParameter(name = "MQ_PASSWORD", value = "${amq.password:redhat}"),
+                @TemplateParameter(name = "MQ_PROTOCOL", value = "openwire,amqp,mqtt,stomp")})
+@RoleBinding(roleRefName = "view", userName = "system:serviceaccount:${kubernetes.namespace}:default")
+@OpenShiftResource("classpath:testrunner-secret.json")
+@OpenShiftResource("classpath:testrunner-secret.json")
+@Replicas(1)
+@Ignore("https://github.com/jboss-openshift/ce-testsite/issues/123")
+public class Amq63VirtualTopicSubscriberMigrationTest extends AmqVirtualTopicSubscriberMigrationTestBase {
+
+    @Deployment
+    public static WebArchive getDeployment() throws IOException {
+        return getDeploymentBase(AmqMigrationTestBase.class, AmqVirtualTopicSubscriberMigrationTestBase.class);
+    }
+}
---
+++ tests/arq/src/test/java/org/jboss/test/arquillian/ce/amq/support/AmqClient.java
@@ -0,0 +1,355 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015 Red Hat Inc. and/or its affiliates and other
+ * contributors as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.test.arquillian.ce.amq.support;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+import javax.naming.NamingException;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.ActiveMQSslConnectionFactory;
+import org.apache.qpid.jms.JmsConnectionFactory;
+import org.apache.qpid.jms.transports.TransportSslOptions;
+import org.fusesource.stomp.jms.StompJmsConnectionFactory;
+
+/**
+ * @author Ricardo Martinelli
+ */
+public class AmqClient {
+    private static final Logger log = Logger.getLogger(AmqClient.class.getName());
+
+    private final String connectionUrl;
+    private final String username;
+    private final String password;
+
+    private static final long MSG_TIMEOUT = 60000;
+
+    public AmqClient(String connectionUrl, String username, String password) throws IOException {
+        this.connectionUrl = connectionUrl;
+        this.username = username;
+        this.password = password;
+    }
+
+    private ConnectionFactory getAMQConnectionFactory(boolean secured) throws Exception {
+        if (secured) {
+            ActiveMQSslConnectionFactory cf = new ActiveMQSslConnectionFactory(this.connectionUrl);
+            cf.setTrustStore(System.getProperty("javax.net.ssl.trustStore"));
+            cf.setTrustStorePassword(System.getProperty("javax.net.ssl.trustStorePassword"));
+
+            return cf;
+        }
+        return new ActiveMQConnectionFactory(this.connectionUrl);
+    }
+
+    private void close(Connection connection) throws JMSException {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    public String consumeOpenWireJms(boolean isSecured) throws Exception {
+        return consumeOpenWireJms(0, isSecured);
+    }
+
+    public String consumeOpenWireJms(long timeout, boolean isSecured) throws Exception {
+        Connection conn = null;
+        try {
+            ConnectionFactory cf = getAMQConnectionFactory(isSecured);
+            conn = createConnection(cf, username, password);
+            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination qFoo = session.createQueue("QUEUES.FOO");
+
+            MessageConsumer consumer = session.createConsumer(qFoo);
+
+            conn.start();
+
+            Message msg = consumer.receive(timeout);
+            if (msg != null) {
+                String text = ((TextMessage) msg).getText();
+                log.warning("Msg: " + text);
+                return text;
+            } else {
+                log.warning("No msgs ...");
+                return null;
+            }
+        } finally {
+            close(conn);
+        }
+    }
+
+    public void consumeOpenWireJms(List<String> msgs, int size, boolean isSecured) throws Exception {
+        Connection conn = null;
+        try {
+            ConnectionFactory cf = getAMQConnectionFactory(isSecured);
+            conn = createConnection(cf, username, password);
+            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination qFoo = session.createQueue("QUEUES.FOO");
+
+            MessageConsumer consumer = session.createConsumer(qFoo);
+
+            conn.start();
+
+            log.warning("Starting open-wire-consume ...");
+            while (size > 0) {
+                Message msg = consumer.receive(MSG_TIMEOUT);
+                if (msg == null) {
+                    throw new IllegalStateException("Missing " + size + " messages.");
+                }
+                msgs.add(((TextMessage) msg).getText());
+                size--;
+            }
+        } finally {
+            close(conn);
+        }
+    }
+
+    public void produceOpenWireJms(String message, boolean isSecured) throws Exception {
+        Connection conn = null;
+        try {
+            ConnectionFactory cf = getAMQConnectionFactory(isSecured);
+            conn = createConnection(cf, username, password);
+            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination qFoo = session.createQueue("QUEUES.FOO");
+
+            MessageProducer producer = session.createProducer(qFoo);
+
+            conn.start();
+
+            Message msg = session.createTextMessage(message);
+            producer.send(msg);
+        } finally {
+            close(conn);
+        }
+    }
+
+    public void produceOpenWireJms(List<String> msgs, boolean isSecured) throws Exception {
+        Connection conn = null;
+        try {
+            ConnectionFactory cf = getAMQConnectionFactory(isSecured);
+            conn = createConnection(cf, username, password);
+            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination qFoo = session.createQueue("QUEUES.FOO");
+
+            MessageProducer producer = session.createProducer(qFoo);
+
+            conn.start();
+
+            for (String message : msgs) {
+                Message msg = session.createTextMessage(message);
+                producer.send(msg);
+            }
+        } finally {
+            close(conn);
+        }
+    }
+
+    public String consumeAmqp() throws JMSException, NamingException {
+        TransportSslOptions options = new TransportSslOptions();
+        options.setTrustStoreLocation(System.getProperty("javax.net.ssl.trustStore"));
+        options.setTrustStorePassword("password");
+        options.setVerifyHost(false);
+        ConnectionFactory factory = new JmsConnectionFactory(connectionUrl);
+
+        Connection connection = createConnection(factory, username, password);
+        try {
+            connection.start();
+
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            Queue q = session.createQueue("QUEUES.BAR");
+            MessageConsumer consumer = session.createConsumer(q);
+
+            Message msg = consumer.receive(MSG_TIMEOUT);
+            if (msg == null) {
+                throw new IllegalStateException("No messages.");
+            }
+
+            return ((TextMessage) msg).getText();
+        } finally {
+            close(connection);
+        }
+    }
+
+    public void produceAmqp(String message) throws JMSException, NamingException {
+        ConnectionFactory factory = new JmsConnectionFactory(connectionUrl);
+
+        Connection conn = createConnection(factory, username, password);
+        try {
+            conn.start();
+
+            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            Queue q = session.createQueue("QUEUES.BAR");
+            MessageProducer producer = session.createProducer(q);
+
+            TextMessage msg = session.createTextMessage(message);
+            producer.send(msg);
+
+            producer.close();
+            session.close();
+        } finally {
+            close(conn);
+        }
+    }
+
+    public String consumeStomp() throws Exception {
+        StompJmsConnectionFactory factory = new StompJmsConnectionFactory();
+        factory.setBrokerURI(connectionUrl);
+        Connection conn = createConnection(factory, username, password);
+        try {
+            conn.start();
+
+            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue q = session.createQueue("QUEUES.FOO");
+
+            MessageConsumer consumer = session.createConsumer(q);
+
+            TextMessage msg = (TextMessage)consumer.receive(MSG_TIMEOUT);
+            if (msg == null) {
+                throw new IllegalStateException("No messages.");
+            }
+            return msg.getText();
+        } finally {
+            close(conn);
+        }
+    }
+
+    public void produceStomp(String message) throws Exception {
+        StompJmsConnectionFactory factory = new StompJmsConnectionFactory();
+        factory.setBrokerURI(connectionUrl);
+        Connection conn = createConnection(factory, username, password);
+        try {
+            conn.start();
+
+            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Queue q = session.createQueue("QUEUES.FOO");
+
+            MessageProducer producer = session.createProducer(q);
+
+            producer.send(session.createTextMessage(message));
+        } finally {
+            close(conn);
+        }
+    }
+
+    public void createVirtualTopicSubscriber(String subscriptionName) throws Exception {
+        Connection conn = null;
+        try {
+            ConnectionFactory cf = getAMQConnectionFactory(false);
+            conn = createConnection(cf, username, password);
+            conn.setClientID("tmp123");
+
+            conn.start();
+
+            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination tFoo = session.createQueue("Consumer." + subscriptionName + ".VirtualTopic.FOO");
+
+            // This may need to remain open
+            MessageConsumer consumer = session.createConsumer(tFoo);
+            consumer.close();
+        } finally {
+            close(conn);
+        }
+    }
+
+    public void produceVirtualTopic(String message) throws Exception {
+        Connection conn = null;
+        try {
+            ConnectionFactory cf = getAMQConnectionFactory(false);
+            conn = createConnection(cf, username, password);
+
+            conn.start();
+
+            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Topic tFoo = session.createTopic("VirtualTopic.FOO");
+            MessageProducer producer = session.createProducer(tFoo);
+            producer.send(session.createTextMessage(message));
+        } finally {
+            close(conn);
+        }
+    }
+
+    public List<String> consumeVirtualTopic(int N, long timeout, String subscriptionName) throws Exception {
+        Connection conn = null;
+        try {
+            ConnectionFactory cf = getAMQConnectionFactory(false);
+            conn = createConnection(cf, username, password);
+            conn.setClientID("tmp123");
+
+            conn.start();
+
+            Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            Destination tFoo = session.createQueue("Consumer." + subscriptionName + ".VirtualTopic.FOO");
+
+            MessageConsumer consumer = session.createConsumer(tFoo);
+            List<String> msgs = new ArrayList<>();
+            while (N > 0) {
+                TextMessage msg = (TextMessage)consumer.receive(timeout);
+                if (msg == null) {
+                    throw new IllegalStateException("Missing " + N + " messages.");
+                }
+                msgs.add(msg.getText());
+                N--;
+            }
+            return msgs;
+        } finally {
+            close(conn);
+        }
+    }
+
+    private Connection createConnection(ConnectionFactory cf, String username, String password) throws JMSException {
+        final int TRIES = 3;
+        final int DELAY = 5;
+
+        for (int i = 1; i <= TRIES; i++) {
+            try {
+                return cf.createConnection(username, password);
+            } catch (JMSException e) {
+                log.warning(String.format("Failed to create an AMQ connection. Attempt #%d of #%d. Caused by: %s", i, TRIES, e.getMessage()));
+                if (i == TRIES)
+                    throw e;
+                try {
+                    Thread.sleep(DELAY * 1000);
+                } catch (InterruptedException e1) {
+                }
+            }
+        }
+
+        return null;
+    }
+}
